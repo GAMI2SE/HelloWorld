f2e93885a37e133e458e01db233c9581f4115234
julie <julie@DESKTOP-QPKII2D>
2017-07-04 22:36:29 -0700

    fourth

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -18,6 +18,9 @@ public class Demo {
      */
     public static void main(String[] args) {
         System.out.println("Hello world");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Cho Vu, Cho Sang");
+        }
     }
     
 }
